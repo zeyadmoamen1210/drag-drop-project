a029c589696632b506c6856662d959c28ac42148
Zoz <zeyadmoamen7@gmail.com>
2023-05-05 17:25:40 +0200

    Delete folder

--- src/scripjs/utils/project_status.js
+++
@@ -1,6 +0,0 @@
-export var ProjectStatus;
-(function (ProjectStatus) {
-    ProjectStatus["Initial"] = "Initial";
-    ProjectStatus["Active"] = "Active";
-    ProjectStatus["Finished"] = "Finished";
-})(ProjectStatus || (ProjectStatus = {}));
